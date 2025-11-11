0db41d9047cd3e0dcc2961960da1fc79aaeea208
Eduardo-cmd0809 <223112018@upmh.edu.mx>
2025-11-11 08:33:17 -0600

    el buen lalo

--- TakIn/src/backend/db.js
+++ TakIn/src/backend/db.js
@@ -3,13 +3,15 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: '123456789',
   database: 'Liberty',
 });
 
 db.connect(err => {
   if (err) {
-    console.error('Error al conectar MySQL:', err);
+    console.error('Error al conectar MySQL:', err.message);
+    console.error('Código de error:', err.code);
+    console.error('Detalles completos:', err);
     return;
   }
   console.log('Conectado a MySQL');
