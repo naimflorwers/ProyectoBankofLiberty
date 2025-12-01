00550e22a35f78ed7389c1012f73d8762ecba1f1
paola <223111939@upmh.edu.mx>
2025-12-01 08:09:49 -0600

    Capacitor instalado faltan archivos

---
+++ TakIn/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.proyectobankofliberty.app',
+  appName: 'Banco',
+  webDir: 'dist/takin/browser'
+};
+
+export default config;
